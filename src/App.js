91bb4fbc7ce428417551a52c616a50de7a534e54
Yari Cherchi <yari.cherchi7@gmail.com>
2023-01-26 10:39:33 +0100

    aggiunto daisyUI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App"> 
         <h1 className="text-3xl font-bold underline">
       Hello world!
-     </h1>
-      </header>
+     </h1> 
     </div>
   );
 }
