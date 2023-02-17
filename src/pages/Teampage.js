4d24941d50cc829b5fb4f28b72fab68170987af0
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-17 12:28:38 +0000

    working on teampage, added first avatar and modified style

--- src/pages/Teampage.js
+++ src/pages/Teampage.js
@@ -1,10 +1,24 @@
 import "../styles/teampage.css";
+import Bianca from "../images/Bianca.jpeg";
 
 export function Teampage() {
   return (
-    <div className="container team-container text-center">
-      <h2 className="team-title"> Flyer<span className="s-flyers italic">S</span></h2>
-      <h3>⏬️ Space to introduce our team 😎 ⏬️</h3>
-    </div>
+      <div className="team-container text-center">
+        <h2 className="team-title ">
+          Flyer<span className="s-flyers italic">S</span>
+        </h2>
+        {/* <h3>⏬️ Space to introduce our team 😎 ⏬️</h3>*/}
+        <div className="avatar-about-container flex flex-row items-center mx-5">
+          <div className="avatar mt-5">
+            <div className="w-32 mask mask-hexagon">
+              <img src={Bianca} alt="Avatar Bianca"/>
+            </div>
+          </div>
+          <div className="about p-10 mt-5 text-left">
+            <h3 className="name-avatar-title font-bold mb-2"> Bianca - Junior Full Stack Developer 🙌🏻</h3>
+                <p>Determinata e curiosa fin da piccola, affronto sempre le sfide con grande entusiasmo, positività e un’incredibile voglia di fare. Così, dopo una formazione da storica dell’Arte, mi sono poi approcciata al mondo della programmazione, che è diventata ormai la mia principale passione. </p>
+          </div>
+        </div>
+      </div>
   );
 }
